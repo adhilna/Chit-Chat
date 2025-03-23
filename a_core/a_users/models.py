388f65c2270ecd12fac455983bd6d32c53f6f9c7
adhilna <adhilchy11@gmail.com>
2025-03-23 15:58:35 +0530

    Added user profile and signals functionality

---
+++ a_core/a_users/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.templatetags.static import static
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    images = models.ImageField(upload_to='avatars/', null=True, blank=True)
+    display_name = models.CharField(max_length=128, null=True, blank=True)
+    info = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.user)
+
+    @property
+    def name(self):
+        if self.display_name:
+            name = self.display_name
+        else:
+            name = self.user.username
+        return name
+    
+    @property
+    def avatar(self):
+        if self.images:
+            avatar = self.images.url
+        else:
+            avatar = static('img/avatar.svg')
+        return avatar
