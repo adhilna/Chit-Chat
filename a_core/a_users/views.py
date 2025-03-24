330ce2b68df73b13d1da282fe438c9040dc701d4
adhilna <adhilchy11@gmail.com>
2025-03-24 11:53:16 +0530

    Added Edit Profile Page with avatar update feature

--- a_core/a_users/views.py
+++ a_core/a_users/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .forms import *
 
 def profile_view(request):
     profile = request.user.profile
     context = {'profile': profile}
     return render(request, 'a_users/profile.html', context)
+
+@login_required
+def profile_edit_view(request):
+    form = ProfileForm(instance=request.user.profile)
+    context = {'form': form}
+    return render(request, 'a_users/profile_edit.html', context)
