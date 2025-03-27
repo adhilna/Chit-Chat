4665b71a9ad6b1e464ef7a8a7470f9a766d16cba
adhilna <adhilchy11@gmail.com>
2025-03-27 10:57:13 +0530

    profile page updated

--- a_core/a_users/views.py
+++ a_core/a_users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import *
 
@@ -10,5 +10,10 @@ def profile_view(request):
 @login_required
 def profile_edit_view(request):
     form = ProfileForm(instance=request.user.profile)
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            return redirect('profile')
     context = {'form': form}
     return render(request, 'a_users/profile_edit.html', context)
